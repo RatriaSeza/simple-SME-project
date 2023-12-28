e4fca3bde6ceba2473f75e230e28b50825d160ba
RatriaSeza <satriareza2002@gmail.com>
2023-12-28 15:41:49 +0700

    create attendances page and factory

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Employee;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,17 @@ class AttendanceFactory extends Factory
      */
     public function definition(): array
     {
+        $date = fake()->date();
+
         return [
-            //
+            'employee_id' => Employee::get()->random()->employee_id,
+            'attendance_date' => $date,
+            'day' => Carbon::parse($date)->format('l'),
+            'time_in' => fake()->dateTimeBetween('08:00:00', '10:00:00')->format('H:i:s'),
+            'time_out' => fake()->dateTimeBetween('15:00:00', '18:00:00')->format('H:i:s'),
+            'break_time_start' => fake()->dateTimeBetween('11:30:00', '12:30:00')->format('H:i:s'),
+            'break_time_end' => fake()->dateTimeBetween('12:30:00', '13:00:00')->format('H:i:s'),
+            'working_hours' => rand(6,12),
         ];
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,6 +13,13 @@
             </div>
             Employee
         </a>
+        <a href="{{ route('attendances') }}"
+            class="flex items-center w-full px-6 py-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900  {{ request()->is('attendances') ? 'bg-gray-900 text-white' : '' }}">
+            <div class="grid mr-4 place-items-center">
+                <i class="fa-solid fa-calendar-days"></i>
+            </div>
+            Attendances
+        </a>
         <div role="button"
             class="flex items-center w-full px-6 py-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
             <div class="grid mr-4 place-items-center">
