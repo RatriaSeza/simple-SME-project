712cedd9bd5960139a3ef206114bf3cf181ee57d
RatriaSeza <satriareza2002@gmail.com>
2023-12-31 00:28:53 +0700

    request validation for attendance controller and fillable field

---
+++ app/Http/Requests/AttendanceControllerRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AttendanceControllerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'attendance_date' => 'required|date',
+            'time_in' => 'nullable',
+            'time_out' => 'nullable',
+            'break_time_start' => 'nullable',
+            'break_time_end' => 'nullable',
+        ];
+    }
+}
--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -10,6 +10,13 @@ class Attendance extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['employee_id', 'day', 'attendance_date', 'time_in', 'time_out', 'break_time_start', 'break_time_end', 'working_hours'];
+
     public function employee(): BelongsTo
     {
         return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
